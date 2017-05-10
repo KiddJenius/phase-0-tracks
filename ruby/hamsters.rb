7289816970233258ecfe2a082bb8b8cd9abdaf2f
Joe Antoun <jantoun@gmail.com>
2017-05-10 13:43:33 -0500

    Add until loop for verdict

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,10 +1,26 @@
 puts "What is the hamsters name?"
 	name = gets.chomp
 puts "How loud is the hampster 1-10?"
-	volume = gets.chomp
+	volume = gets.chomp.to_i
 puts "What color fur does it have?"
 	fur = gets.chomp
-puts "Is this a good candidate for adoption?"
-	verdict =  gets.chomp
-puts = "What is the hampster's age?"
+verdict = nil
+until verdict != nil
+puts "Is this a good candidate for adoption (y/n)?"
+	verdict = gets.chomp.downcase
+	if verdict == "y"
+		verdict = true
+	elsif verdict == "n"
+		verdict = false
+	else 
+		verdict = nil
+	end
+end
+
+puts "What is the hampster's age?"
 	age = gets.chomp
+if age == ""
+	age = nil
+end
+
+p age
