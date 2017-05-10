d30e86611b617f34f6c8fcec5e9b6bcf197ac486
Joe Antoun <jantoun@gmail.com>
2017-05-10 18:13:11 -0500

    Decrypt code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,12 +1,26 @@
-def encrypt() 
-	puts "Give me a word to encrypt"
+# def encrypt() 
+# 	puts "Give me a word to encrypt"
+# 	str = gets.chomp
+# 	counter = 0
+# 	while counter < str.length
+# 		str[counter]= str[counter].next
+# 		counter += 1
+# 	end
+# 	puts str
+# end
+
+# encrypt()
+
+def decrypt()
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	puts "Give me a word to decrypt"
 	str = gets.chomp
 	counter = 0
 	while counter < str.length
-		str[counter]= str[counter].next
-		counter += 1
+		str[counter]= alphabet[alphabet.index(str[counter])-1]
+		counter +=1
 	end
 	puts str
 end
 
-encrypt()
+decrypt()
