c2ab22cd63a41f16b2cb0bcf281596920b5a6980
Joe Antoun <jantoun@gmail.com>
2017-05-10 13:53:27 -0500

    Print questions to console, test code.  works

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,6 +1,6 @@
 puts "What is the hamsters name?"
 	name = gets.chomp
-puts "How loud is the hampster 1-10?"
+puts "How loud is the hamster 1-10?"
 	volume = gets.chomp.to_i
 puts "What color fur does it have?"
 	fur = gets.chomp
@@ -17,10 +17,16 @@ puts "Is this a good candidate for adoption (y/n)?"
 	end
 end
 
-puts "What is the hampster's age?"
+puts "What is the hamster's age?"
 	age = gets.chomp
 if age == ""
 	age = nil
+else
+	age.to_i
 end
 
-p age
+puts "The new hamsters name is #{name}."
+puts "On a scale of 1-10, the hamsters loudness is #{volume}."
+puts "The fur color is #{fur}."
+puts "Is he a good candidate for adoption? #{verdict}"
+puts "The hamster in question is #{age} years old."
