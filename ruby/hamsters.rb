fa8f437a1ce38df5ff6846794bd0cf4ab23ee521
Joe Antoun <jantoun@gmail.com>
2017-05-10 13:21:32 -0500

    Initial commit add 5 questions

---
+++ ruby/hamsters.rb
@@ -0,0 +1,10 @@
+puts "What is the hamsters name?"
+	name = gets.chomp
+puts "How loud is the hampster 1-10?"
+	volume = gets.chomp
+puts "What color fur does it have?"
+	fur = gets.chomp
+puts "Is this a good candidate for adoption?"
+	verdict =  gets.chomp
+puts = "What is the hampster's age?"
+	age = gets.chomp
