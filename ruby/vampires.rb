3b0a1ea3e752ce4f98b594379846ee5f934f50e6
Joe Antoun <jantoun@gmail.com>
2017-05-11 18:07:46 -0500

    Release 1-4 complete

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,17 +1,71 @@
-puts "So what's your name?"
-name = gets.chomp
+puts "How many employees are going through this racket?"
+candidates = gets.chomp.to_i
 
-current_year= 2017
-def true_age
-	puts "How old are you"
-	age = gets.chomp.to_i
-	puts "What year were you born?"
-	birthyear = gets.chomp.to_i
-		if current_year - birthyear == age
-			age == true
-		else
-			puts "The math doesn't add up.  Are you telling the truth?"
-		end
+def survey
+  puts "So what's your name?"
+  name = gets.chomp.downcase
 
+  puts "How old are you?"
+    given_age = gets.chomp.to_i
+  puts "In what year were you born?"
+    birthyear = gets.chomp.to_i
+
+  current_year = 2017
+  if current_year - birthyear == given_age
+    puts "That math checks out."
+  else
+    puts "Hmmm, the math on that doesn't seem right."
+  end
+
+  puts "Our cafeteria serves garlic bread, should we get you some, yes or no?"
+  bread = gets.chomp.downcase
+
+  puts "Are you going to enroll in our health insurance, yes or no?"
+  health_ins = gets.chomp.downcase
+
+  puts "List any alergies you have.  If none put 'none'.  Put 'done' when done."
+  algery_input = false
+  until algery_input
+  alergies = gets.chomp
+    if alergies == "done" || alergies =="none"
+      puts "Thanks!!"
+      algery_input = TRUE
+    else 
+      puts "Any more?"
+    end
+
+    if current_year - birthyear == given_age
+      testimony = TRUE
+    else 
+      testimony = FALSE
+    end
+  end
+
+  if name == "drake cula" || name == "tu fang"
+    puts "Definately a vampire."
+  elsif testimony == TRUE && (bread == "yes" || health_ins == "yes")
+    puts "Probably not a vampire."
+  elsif testimony == FALSE && (bread == "no" && health_ins == "yes") || (bread == "yes" && health_ins == "no")
+    puts "Probably a vampire"
+  elsif testimony == FALSE && bread == "no" && health_ins == "no"
+    puts "Almost certainly a vampire"
+  else
+    puts "Results inconclusive"
+  end
 end
-true_age
+until candidates == 0
+  survey
+  candidates= candidates - 1
+  puts "#{candidates} left to process."
+end
+
+
+
+
+
+
+
+
+
+
+	
