a682eba5917baa0f3c7e05e73853f21dee03e0f6
Joe Antoun <jantoun@gmail.com>
2017-05-13 12:25:39 -0500

    Add break for sunshine. Final proj technically done.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,6 +1,8 @@
+#Since this number is tied to the whole survey, get it early
 puts "How many employees are going through this racket?"
 candidates = gets.chomp.to_i
 
+# method for the entire survey
 def survey
   puts "So what's your name?"
   name = gets.chomp.downcase
@@ -10,6 +12,7 @@ def survey
   puts "In what year were you born?"
     birthyear = gets.chomp.to_i
 
+# There's probably a better way to auto update the current year.  This if/elsif block references several areas in the program to see if age and given birthyear line up
   current_year = 2017
   if current_year - birthyear == given_age
     puts "That math checks out."
@@ -23,49 +26,47 @@ def survey
   puts "Are you going to enroll in our health insurance, yes or no?"
   health_ins = gets.chomp.downcase
 
+# After general questions the program loops until "none" or "done" end the loop
   puts "List any alergies you have.  If none put 'none'.  Put 'done' when done."
   algery_input = false
   until algery_input
-  alergies = gets.chomp
+  alergies = gets.chomp.downcase
     if alergies == "done" || alergies =="none"
       puts "Thanks!!"
-      algery_input = TRUE
+      algery_input = true
+    elsif alergies == "sunshine"
+      alergy_input = true
+      break
     else 
       puts "Any more?"
     end
-
-    if current_year - birthyear == given_age
-      testimony = TRUE
-    else 
-      testimony = FALSE
-    end
   end
 
+# basically defines the truth
+    if current_year - birthyear == given_age
+      testimony = true
+    else 
+      testimony = false
+    end
+
+# if/elsif block to make a reco on the likelyhood of vampire status
   if name == "drake cula" || name == "tu fang"
     puts "Definately a vampire."
-  elsif testimony == TRUE && (bread == "yes" || health_ins == "yes")
+  elsif testimony == true && (bread == "yes" || health_ins == "yes")
     puts "Probably not a vampire."
-  elsif testimony == FALSE && (bread == "no" && health_ins == "yes") || (bread == "yes" && health_ins == "no")
+  elsif testimony == false && (bread == "no" && health_ins == "yes") || (bread == "yes" && health_ins == "no") || alergies == "sunshine"
     puts "Probably a vampire"
-  elsif testimony == FALSE && bread == "no" && health_ins == "no"
+  elsif testimony == false && bread == "no" && health_ins == "no"
     puts "Almost certainly a vampire"
   else
     puts "Results inconclusive"
   end
 end
+
+# the method in a loop that exits when all candidates are in the system
 until candidates == 0
   survey
   candidates= candidates - 1
   puts "#{candidates} left to process."
 end
 
-puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
-
-
-
-
-
-
-
-
-	
