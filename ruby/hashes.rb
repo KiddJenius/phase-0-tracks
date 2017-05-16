b58c7497624413d98622cd3f64d3518746104f2b
Joe Antoun <jantoun@gmail.com>
2017-05-16 11:03:00 -0500

    Initial commit

---
+++ ruby/hashes.rb
@@ -0,0 +1,17 @@
+#Bacic information from the user
+#Name, age, number of children, annual income
+#Give 4 different wood option types:
+#Walnut, Padauk, Lyptus, Maple
+#Give 5 different artist types for theme, pick 2
+#Pollock, Dali, Warhol, Picasso, Kline
+#Ask do you want carpet in the bedrooms? Yes or No
+
+design_qaire = {
+	:name 
+	:age
+	:numOfChildren
+	:woodType
+	:artistType
+	:carpet
+
+}
