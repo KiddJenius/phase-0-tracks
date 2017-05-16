580fdf448db05b900ee62fbdf15bf70d528a2a31
Joe Antoun <jantoun@gmail.com>
2017-05-16 17:02:53 -0500

    Initial commit

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,10 @@
+#universe is the hash
+#quasar, galaxy, empty space, 
+#solar system, star, black hole, nebula
+#planet, comet, moon
+
+universe {
+	'quasar' => ['dust', 'gasses'],
+	'galaxy' => ['solar system'{ planet: earth, comet: haley moon: ganymede}, 'black hole', 'star', 'nebula']
+	'empty space' => ['vacuum', 'dark matter']
+}
