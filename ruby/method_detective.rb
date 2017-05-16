d108da6d9092cc3a7295989c8a3f3aeca611cdc1
Joe Antoun <jantoun@gmail.com>
2017-05-15 21:30:00 -0500

    Add ul after all questions

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -15,35 +15,36 @@
 => “zoom”
 "cra-cra".insert(3, 'y')
 => "cray-cra"
-
+---------------------------------------------------------------------
 "enhance".center(15)
 => "    enhance    "
- 
+ --------------------------------------------------------------------
 
 "Stop! You’re under arrest!".upcase
 => "STOP! YOU’RE UNDER ARREST!"
-
+---------------------------------------------------------------------
 "the usual".insert(-1, " suspects")
 => "the usual suspects"
-
+---------------------------------------------------------------------
 " suspects".insert(0, "the usual")
 => "the usual suspects"
-
+---------------------------------------------------------------------
 "The case of the disappearing last letter".chomp('r')
 "The case of the disappearing last letter".chop
 => "The case of the disappearing last lette"
-
+---------------------------------------------------------------------
 
 "The mystery of the missing first letter".delete "T"
 => "he mystery of the missing first letter"
-
+---------------------------------------------------------------------
 "Elementary,    my   dear        Watson!".squeeze(" ")
 => "Elementary, my dear Watson!"
-
+---------------------------------------------------------------------
 "z".ord
 => 122 
 (What is the significance of the number 122 in relation to the character z?)
 #Actually my first thought was 122 is the ASCII of z, which is correct, but it also connects that z is the ordinal integer of 122.
-
+---------------------------------------------------------------------
 "How many times does the letter 'a' appear in this string?".count('a')
 => 4
+----------------------------------------------------------------------
