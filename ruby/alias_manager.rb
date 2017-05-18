b7e2134aec9834547a12412b43cfe8fead8a9995
Joe Antoun <jantoun@gmail.com>
2017-05-18 18:30:15 -0500

    Convert switched name to string, make each char into array

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,5 +3,10 @@ spy_name = "Jonathan Pollard"
 #split name into array of items
 spy_name =spy_name.split(' ')
 #reverse order of array items
-spy_name.reverse
-
+spy_name.reverse!
+#return swapped name as string
+newname = spy_name.join(' ')
+newname = spy_name.join(' ')
+#split swapped name into array as individual letters
+new_arry = newname.split('')
+p new_arry
