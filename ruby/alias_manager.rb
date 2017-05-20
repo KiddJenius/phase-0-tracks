c94391bf5e4a9a5478e7122c84a6908118a2b985
Joe Antoun <jantoun@gmail.com>
2017-05-20 08:59:32 -0500

    Copy over from repl code prints translated name correctly

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,7 +6,35 @@ spy_name =spy_name.split(' ')
 spy_name.reverse!
 #return swapped name as string
 newname = spy_name.join(' ')
-newname = spy_name.join(' ')
 #split swapped name into array as individual letters
-new_arry = newname.split('')
-p new_arry
+swap_arry = newname.split('')
+#change all vowel items to next vowel in series
+swap_arry.each do |indivLetter| 
+  if indivLetter == "a"
+    indivLetter.replace("e")
+  elsif indivLetter == "e"
+    indivLetter.replace("i")
+  elsif indivLetter == "i"
+    indivLetter.replace("o")
+  elsif indivLetter == "o"
+    indivLetter.replace("u")
+  elsif indivLetter == "u"
+    indivLetter.replace("a")
+  end
+end
+
+#for every letter that's not "aeiou":
+swap_arry.each do |char|
+  if char != "a" && char != "e" && char != "i" && char != "o" && char != "u"
+     char.next!
+  end
+end
+#change exclimation points to spaces
+swap_arry.each do |point|
+  if point == ("!")
+    point.replace(" ")
+  end
+end
+
+final_string = swap_arry.join('')
+puts final_string
