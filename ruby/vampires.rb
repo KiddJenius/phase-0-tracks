7d92ae140fd9c9b4e537d938e8bb8aaabb802f6a
Joe Antoun <jantoun@gmail.com>
2017-05-22 20:14:15 -0500

    Refactor Time.now and result variable update

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -13,7 +13,7 @@ def survey
     birthyear = gets.chomp.to_i
 
 # There's probably a better way to auto update the current year.  This if/elsif block references several areas in the program to see if age and given birthyear line up
-  current_year = 2017
+  current_year = Time.new.year
   if current_year - birthyear == given_age
     puts "That math checks out."
   else
@@ -22,24 +22,18 @@ def survey
 
   puts "Our cafeteria serves garlic bread, should we get you some, yes or no?"
   bread = gets.chomp.downcase
+  likes_garlic = bread == "y"
 
   puts "Are you going to enroll in our health insurance, yes or no?"
   health_ins = gets.chomp.downcase
+  wants_ins = health_ins == "y"
 
 # After general questions the program loops until "none" or "done" end the loop
   puts "List any alergies you have.  If none put 'none'.  Put 'done' when done."
-  algery_input = false
-  until algery_input
   alergies = gets.chomp.downcase
-    if alergies == "done" || alergies =="none"
-      puts "Thanks!!"
-      algery_input = true
-    elsif alergies == "sunshine"
-      alergy_input = true
-      break
-    else 
+  until alergies == "done" || alergies == "sunshine"
       puts "Any more?"
-    end
+      alergies = gets.chomp.downcase
   end
 
 # basically defines the truth
@@ -50,16 +44,24 @@ def survey
     end
 
 # if/elsif block to make a reco on the likelyhood of vampire status
+  
+  if testimony == true && (bread == "yes" || health_ins == "yes")
+    result = "Probably not a vampire."
+  end
+  if testimony == false && (bread == "no" && health_ins == "yes") || (bread == "yes" && health_ins == "no") || alergies == "sunshine"
+    result = "Probably a vampire"
+  end
+  if testimony == false && bread == "no" && health_ins == "no"
+    result = "Almost certainly a vampire"
+  end
   if name == "drake cula" || name == "tu fang"
-    puts "Definately a vampire."
-  elsif testimony == true && (bread == "yes" || health_ins == "yes")
-    puts "Probably not a vampire."
-  elsif testimony == false && (bread == "no" && health_ins == "yes") || (bread == "yes" && health_ins == "no") || alergies == "sunshine"
-    puts "Probably a vampire"
-  elsif testimony == false && bread == "no" && health_ins == "no"
-    puts "Almost certainly a vampire"
-  else
+    result = "Definately a vampire."
+  end
+  if result == nil
     puts "Results inconclusive"
+  end
+  if result != nil
+    puts result
   end
 end
 
