957402b7fa0974af26031efab1810ad1d190ae82
Joe Antoun <jantoun@gmail.com>
2017-05-24 17:26:02 -0500

    Final code to subl

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,6 +1,8 @@
-
 class Puppy
-
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
+  
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -30,3 +32,46 @@ bear.fetch("bone")
 bear.speak(3)
 bear.rollover
 bear.dog_years(11)
+
+class Weather
+  
+  def initialize
+    puts "Initialized..."
+  end
+  
+  def wind
+    puts "This must be Chicago"
+  end
+  
+  def fog 
+    puts "This must be SF"
+  end
+
+
+end
+
+today = Weather.new 
+today.wind
+today.fog 
+
+weather_options = []
+4.times do |x|
+  x = Weather.new 
+  weather_options << x 
+end 
+p weather_options
+
+weather_options.each do |y|
+  y.wind
+  y.fog
+end
+
+
+
+
+
+
+
+
+
+
