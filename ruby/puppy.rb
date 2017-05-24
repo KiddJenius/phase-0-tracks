aa04276a5b8edc96e4396075c2b7c27ee4b36afc
Joe Antoun <jantoun@gmail.com>
2017-05-24 06:11:42 -0500

    Initial commit

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+# Puppy ------------------------
+# labrador retriever
+
+# CHARACTERISTICS ----------------
+# Color: black, brown, white
+# type: English, American
+# energy: high
+# Name: varies
+# 
+
+# BEHAVIOR -----------------------
+# fetch
+# chew
+# tug 
+
+class Puppies
+end
+
+duchess = Puppies.new 
+fido = Puppies.new
+spot = Puppies.new
