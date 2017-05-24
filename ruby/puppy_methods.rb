c4dc44e958824ff5a994f98f3c87327c67d07428
Joe Antoun <jantoun@gmail.com>
2017-05-24 16:59:24 -0500

    Add driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,8 +1,32 @@
+
 class Puppy
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak(integer)
+    puts "Woof " * integer
+  end
+  
+  def rollover
+    puts "Roll over!"
+  end
+  
+  def dog_years(human_years)
+    dog_age = human_years*7
+    p dog_age
+  end
+  
+  def eat_shoes
+    shoes_eaten = true 
+  end
+  
 end
 
+bear = Puppy.new
+bear.fetch("bone")
+bear.speak(3)
+bear.rollover
+bear.dog_years(11)
