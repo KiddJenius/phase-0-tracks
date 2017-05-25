5c6ab5c6f83abcd501c0827c938b32c206e38772
Joe Antoun <jantoun@gmail.com>
2017-05-24 19:34:34 -0500

    Shout module with driver code complete

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,32 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yell_happily(words)
-  	words + "Happy happy " + "Joy-joy!"
+#   def self.yell_happily(words)
+#   	words + "Happy happy " + "Joy-joy!"
+#   end
+# end
+
+# puts Shout.yell_angrily("Damn")
+# puts Shout.yell_happily("Ok everybody now ")
+
+module Shout
+  def Uh_oh(bad_name)
+    puts "Get your butt over here right now #{bad_name}!!!"
   end
 end
 
-puts Shout.yell_angrily("Damn")
-puts Shout.yell_happily("Ok everybody now ")
+class Boss
+	include Shout
+end
+
+class Mother
+	include Shout
+end
+
+boss = Boss.new
+boss.Uh_oh("slacker")
+
+mother = Mother.new
+mother.Uh_oh("young man")
