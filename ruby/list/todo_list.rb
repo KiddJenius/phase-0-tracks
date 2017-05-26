947217b37f1e1ae5aaf66acceca25df404662898
Joe Antoun <jantoun@gmail.com>
2017-05-26 00:43:54 -0500

    Redo project on new feature branch

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -7,3 +7,20 @@ class TodoList
 	def get_items
 		@list
 	end
+
+	def add_item(item)
+		@list << "mop"
+	end
+
+	def delete_item(item)
+		@list.delete("do the dishes")
+		@list
+	end
+
+	def get_item(index)
+		@list[index]
+	end
+end
+
+
+
