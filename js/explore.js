49fa6a12f5db75444409cbab3ecb7e49de234278
Joe Antoun <jantoun@gmail.com>
2017-06-02 07:34:53 -0500

    Wrap code in function, add final == condition

--- js/explore.js
+++ js/explore.js
@@ -3,12 +3,26 @@ assign the index number of each letter of the anchor word.
 start with the highest index number and list the remaining numbers by index largest to smallest.
 store and print the result */
 
-var anchor = "unicorn"
-var newWord = ""
-for (var i = anchor.length - 1; i >= 0; i--){
-	console.log(anchor[i])
-  newWord += anchor[i] 
+function reverseWord(anchor) {
+  var newWord = ""
+	for (var i = anchor.length - 1; i >= 0; i--){
+		console.log(anchor[i])
+  	newWord += anchor[i] 
+	}
+  return newWord
 }
 
-console.log (newWord)
+
+// 1. call the function to reverse a string
+// 2. store that result as a variable
+// 3. print variable if some condition is true
+
+
+var choice = reverseWord("lollipop");
+
+//is this what they were asking for?
+
+if (1==1){ 
+  console.log(choice);
+}
 
