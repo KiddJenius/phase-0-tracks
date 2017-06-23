1b6a8c2b4e631c97749a531f57d59316ddaa38f4
Joe Antoun <jantoun@gmail.com>
2017-06-23 13:46:03 -0500

    Wrap survey in loop, add counter for number of candidates

--- ruby/vampires2.rb
+++ ruby/vampires2.rb
@@ -1,43 +1,52 @@
-puts "So.. what's your name?"
-name = gets.chomp.downcase
-puts "How old are you?"
-given_age = gets.chomp.to_i 
-puts "Ok.  Then what year were you born?"
-given_birthyear = gets.chomp.to_i  
+puts "How many candidates are we processing here?"
+candidates = gets.chomp.to_i
 
-
-if Time.new.year - given_birthyear == given_age
-  puts "That math checks out.  Hi #{name}."
-  honest = true 
-else
-  puts "Hmmm, that math dosen't check out....\"#{name.upcase}\"."
-  honest = false 
-end 
-
-puts "Our company cafeteria serves garlic bread.  Should we get you some? (Y/N)"
-bread = gets.chomp.downcase 
-if bread == "y"
-  likesbread = true 
-else
-  likesbread = false
-end 
-
-puts "Would you like to enroll in the companies health insurance? (Y/N)"
-health_ins = gets.chomp.downcase 
-if health_ins == "y"
-  needs_ins = true 
-else 
-  needs_ins = false
-end 
-
-if name == "tu fang" || name == "drake cula"
-  puts "Definately a vampire."
-elsif honest && likesbread && needs_ins
-  puts "Probably not a vampire."
-elsif !honest && (!likesbread && needs_ins) || (!needs_ins && likesbread)
-  puts "Probably a vampire."
-elsif !honest && !likesbread && !needs_ins
-  puts "Almost certainly a vampire!"
-else 
-  puts "Results inconclusive."
+until candidates == 0 
+  puts "So.. what's your name?"
+  name = gets.chomp.downcase
+  
+  puts "How old are you?"
+  given_age = gets.chomp.to_i 
+  
+  puts "Ok.  Then what year were you born?"
+  given_birthyear = gets.chomp.to_i  
+  
+  
+  if Time.new.year - given_birthyear == given_age
+    puts "That math checks out.  Hi #{name}."
+    honest = true 
+  else
+    puts "Hmmm, that math dosen't check out....\"#{name.upcase}\"."
+    honest = false 
+  end 
+  
+  puts "Our company cafeteria serves garlic bread.  Should we get you some? (Y/N)"
+  bread = gets.chomp.downcase 
+  if bread == "y"
+    likesbread = true 
+  else
+    likesbread = false
+  end 
+  
+  puts "Would you like to enroll in the companies health insurance? (Y/N)"
+  health_ins = gets.chomp.downcase 
+  if health_ins == "y"
+    needs_ins = true 
+  else 
+    needs_ins = false
+  end 
+  
+  if name == "tu fang" || name == "drake cula"
+    puts "Definately a vampire."
+  elsif honest && likesbread && needs_ins
+    puts "Probably not a vampire."
+  elsif !honest && (!likesbread && needs_ins) || (!needs_ins && likesbread)
+    puts "Probably a vampire."
+  elsif !honest && !likesbread && !needs_ins
+    puts "Almost certainly a vampire!"
+  else 
+    puts "Results inconclusive."
+  end 
+  candidates -= 1
+  puts "#{candidates} left to process"
 end 
