518574b7594225901be6a229301f4c515bbd3e9c
Joe Antoun <jantoun@gmail.com>
2017-07-18 17:20:10 -0500

    Add conditional to print function

--- js/explore.js
+++ js/explore.js
@@ -1,3 +1,7 @@
+/* get the anchor word to work with and store it in a variable
+assign the index number of each letter of the anchor word.
+start with the highest index number and list the remaining numbers by index largest to smallest.
+store and print the result */
 
 function ReverseWord(baseword) {
   var newStr = ""
