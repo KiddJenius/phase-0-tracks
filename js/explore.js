1bb7fbd8c4919d1099315132e109f8af924a10fe
Joe Antoun <jantoun@gmail.com>
2017-07-18 17:26:52 -0500

    Clean up pseudocode

--- js/explore.js
+++ js/explore.js
@@ -1,7 +1,7 @@
 /* get the anchor word to work with and store it in a variable
 assign the index number of each letter of the anchor word.
 start with the highest index number and list the remaining numbers by index largest to smallest.
-store and print the result */
+store and print the result. */
 
 function ReverseWord(baseword) {
   var newStr = ""
@@ -15,3 +15,4 @@ var stored = ReverseWord("unicorn")
 if (1==1) {	
 	console.log(stored);
 }
+ 
