8442faee59e13293937a6fab2a89609e120c122e
Joe Antoun <jantoun@gmail.com>
2017-07-20 17:25:02 -0500

    Add .each with hashy hash

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -81,6 +81,10 @@ california.virus_effects
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
 
-
+STATE_DATA.each do |state, density|
+  density.each do | value |
+    p value
+  end
+end
 #=======================================================================
 # Reflection Section
