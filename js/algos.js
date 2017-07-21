a910bef93111b800522dd1fd47b7387ddcea4038
Joe Antoun <jantoun@gmail.com>
2017-07-21 13:01:21 -0500

    Add second list to test function

--- js/algos.js
+++ js/algos.js
@@ -1,16 +1,19 @@
-function longestPhrase(listOfPhrases) {
+// 1. Initialize placeholder
+// 2. Compaer length of first item in array to current value in placeholder
+// 3. If item length is greater than what's in the placeholder, replace with item, repeat until end of list
 
-  // 1. Initialize placeholder
-  // 2. Compaer length of first item in array to current value in placeholder
-  // 3. If item length is greater than what's in the placeholder, replace with item, repeat until end of list
-	
+function longestPhrase(listOfPhrases) {	
   var longestWord = "";
   for (var i = 0; i<= listOfPhrases.length - 1; i++) {
     if (listOfPhrases[i].length > longestWord.length) {
     	longestWord = listOfPhrases[i]
     }
   }
-  return longestWord; 
+  console.log(longestWord);
+  return longestWord;   
 }
 
-longestPhrase(["long phrase","longest phrase","longer phrase"])
+var listOfPhrases = ["the long phrase","the longest phrase","the longer phrase"]
+var testPharse = ["a", "bc", "defghijk", "lmnopq", "rstuvw", "xyz"]
+longestPhrase(listOfPhrases);
+longestPhrase(testPharse)
