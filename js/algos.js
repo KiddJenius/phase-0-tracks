8c66062640e31297115433f12698ad75d914a20f
Joe Antoun <jantoun@gmail.com>
2017-07-24 08:15:09 -0500

    Clean up code

--- js/algos.js
+++ js/algos.js
@@ -1,5 +1,5 @@
 // 1. Initialize placeholder
-// 2. Compaer length of first item in array to current value in placeholder
+// 2. Compare length of first item in array to current value in placeholder
 // 3. If item length is greater than what's in the placeholder, replace with item, repeat until end of list
 
 function longestPhrase(listOfPhrases) {	
