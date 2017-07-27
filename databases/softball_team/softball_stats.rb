7b881912dbbe08904e4d934c1e5771c519cd051f
Joe Antoun <jantoun@gmail.com>
2017-07-27 16:34:36 -0500

    Add columns name, bat order, bat avg, position, available

--- databases/softball_team/softball_stats.rb
+++ databases/softball_team/softball_stats.rb
@@ -1,3 +1,16 @@
 require 'sqlite3'
 
 complete_stats = SQLite3::Database.new("stats.db")
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS lineup(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    bat_order INT,
+    bat_avg INT,
+    position VARCHAR(255),
+    available BOOLEAN 
+  )
+SQL
+
+complete_stats.execute(create_table_cmd)
