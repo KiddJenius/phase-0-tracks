945fb5c6b4c572a214ad5c3f2ce4b6d18dd0aa8c
Joe Antoun <jantoun@gmail.com>
2017-07-27 17:10:04 -0500

    Add self as first entry to database

--- databases/softball_team/softball_stats.rb
+++ databases/softball_team/softball_stats.rb
@@ -14,3 +14,9 @@ create_table_cmd = <<-SQL
 SQL
 
 complete_stats.execute(create_table_cmd)
+
+def create_player(complete_stats, player_name, order, avg, field_pos, can_play)
+	complete_stats.execute("INSERT INTO lineup (name, bat_order, bat_avg, position, available) VALUES (?,?,?,?,?)", [player_name, order, avg, field_pos, can_play])
+end
+
+create_player(complete_stats,"Joe Antoun", 7, 0.414, "right field", "true")
